dff35af31f598c62fcb14da84a80a0c77e3f6a03
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-07-21 10:33:30 -0500

    7/21

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -4,10 +4,20 @@ namespace RestorationCompanyPortal.Controllers
 {
     public class HelloController : Controller
     {
+        [HttpGet]
+        [Route("/helloworld")]
         public IActionResult Index()
         {
             string html = "<h1>" + "Hello Worlds" + "</h1>";
             return Content(html, "text/html");
+        }
+
+        //GET : /<controller>/welcome
+        [HttpGet]
+        [Route("/helloworld/welcome/{name?}")]
+        public IActionResult Welcome(string name = "World")
+        {
+            return Content("<h1>Welcome to my App, " + name + "!</h1>", "text/html");
         }
     }
 }
