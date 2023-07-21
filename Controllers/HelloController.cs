f75a329d9401d4d200cefaf5b48b863ed7e2b4aa
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-07-21 10:20:10 -0500

    7/21

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestorationCompanyPortal.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            string html = "<h1>" + "Hello Worlds" + "</h1>";
+            return Content(html, "text/html");
+        }
+    }
+}
