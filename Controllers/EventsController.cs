97180fc30b96e8d844469990f3470fcd26dd85b9
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-07-21 12:09:31 -0500

    7/21

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestorationCompanyPortal.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<string> Events = new List<string>();
+
+            Events.Add("Code with Pride");
+            Events.Add("Strange Loop");
+            Events.Add("Women Who code");
+
+            ViewBag.events = Events;
+            return View();
+        }
+    }
+}
