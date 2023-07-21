88d1a17cdd4f150528ec1115d8b56644c254c7fc
Elizabeth <92605769+ebeckerle@users.noreply.github.com>
2023-07-21 10:44:45 -0500

    7/21

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -4,17 +4,37 @@ namespace RestorationCompanyPortal.Controllers
 {
     public class HelloController : Controller
     {
+        //[HttpGet]
+        //[Route("/helloworld")]
+        //public IActionResult Index()
+        //{
+        //    string html = "<h1>" + "Hello Worlds" + "</h1>";
+        //    return Content(html, "text/html");
+        //}
+
         [HttpGet]
         [Route("/helloworld")]
         public IActionResult Index()
         {
-            string html = "<h1>" + "Hello Worlds" + "</h1>";
+            string html = "<form method='post' action='/helloworld/welcome'>"+
+                "<input type='text' name='name' />"+
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
             return Content(html, "text/html");
         }
 
-        //GET : /<controller>/welcome
-        [HttpGet]
-        [Route("/helloworld/welcome/{name?}")]
+        ////GET : /<controller>/welcome
+        //[HttpGet]
+        //[Route("/helloworld/welcome/{name?}")]
+        //public IActionResult Welcome(string name = "World")
+        //{
+        //    return Content("<h1>Welcome to my App, " + name + "!</h1>", "text/html");
+        //}
+
+
+        //POST : /<controller>/welcome
+        [HttpPost]
+        [Route("/helloworld/welcome")]
         public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my App, " + name + "!</h1>", "text/html");
